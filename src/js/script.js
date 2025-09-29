6d36953c583f5728998d2c42f1fbe91c391b86db
Enrico <vincentwelles@protonmail.com>
2025-09-28 23:21:24 -0300

    Por favor colocar css no arquivo dedicado

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,11 @@
-const idadeStr = prompt("Qual a sua idade?");
-if (idadeStr !== null) {
-  const idade = parseInt(idadeStr, 10);
-  if (isNaN(idade)) {
-    alert("Por favor, digite um número válido.");
-  } else if (idade < 18) {
-    alert("Você é menor de idade.");
-  } else {
-    alert("Bem-vindo! Você é maior de idade.");
-  }
-}
+// const idadeStr = prompt("Qual a sua idade?");
+// if (idadeStr !== null) {
+  // const idade = parseInt(idadeStr, 10);
+  // if (isNaN(idade)) {
+    // alert("Por favor, digite um número válido.");
+  // } else if (idade < 18) {
+    // alert("Você é menor de idade.");
+  // } else {
+    // alert("Bem-vindo! Você é maior de idade.");
+  // }
+// } 
